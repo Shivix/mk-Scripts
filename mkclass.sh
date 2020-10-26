b191f50b97d0dd98c11df4f87c1b3d02ff683a7f
Shivix <markoborne1@gmail.com>
2020-10-26 12:17:10 +0000

    fixed indenting of cpp header + added argument to create python classes

--- mkclass.sh
+++ mkclass.sh
@@ -1,13 +1,13 @@
 #!/usr/bin/env bash
 
-if [[ $2 = '-cpp' ]]; then # Create a C++ class
+if [[ $2 = "-cpp" ]]; then # Create a C++ class
 	touch "$1.cpp"
 	touch "$1.hpp"
 	echo "#include \"$1.hpp\"" >> "$1.cpp"
-	printf "#ifndef %s_HPP\n
-	        #define %s_HPP\n\n
-	        class %s {\n
-	        \n
-	        }\n
-	        #endif //%s_HPP" "$1" "$1" "$1" "$1" >> "$1.hpp"
+	printf "#ifndef %s_HPP\n#define %s_HPP\n\nclass %s {\n\n}\n\n#endif //%s_HPP" "$1" "$1" "$1" "$1" >> "$1.hpp"
 fi 
+
+if [[ $2 = "-py" ]]; then
+  touch "$1.py" 
+  echo "class $1:" >> "$1.py"
+fi
