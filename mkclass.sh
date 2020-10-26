0dc0555d0bf9717de97c1aa1004531d728aebc7c
Shivix <markoborne1@gmail.com>
2020-10-26 12:33:46 +0000

    capitalized variable within include guards

--- mkclass.sh
+++ mkclass.sh
@@ -4,7 +4,7 @@ if [[ $2 = "-cpp" ]]; then # Create a C++ class
 	touch "$1.cpp"
 	touch "$1.hpp"
 	echo "#include \"$1.hpp\"" >> "$1.cpp"
-	printf "#ifndef %s_HPP\n#define %s_HPP\n\nclass %s {\n\n}\n\n#endif //%s_HPP" "$1" "$1" "$1" "$1" >> "$1.hpp"
+	printf "#ifndef %s_HPP\n#define %s_HPP\n\nclass %s {\n\n}\n\n#endif //%s_HPP" "${1^^}" "${1^^}" "$1" "${1^^}" >> "$1.hpp"
 fi 
 
 if [[ $2 = "-py" ]]; then
