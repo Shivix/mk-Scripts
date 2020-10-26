67793bfaf1f7508081e758aa9a8aaa17985a81d3
Shivix <markoborne1@gmail.com>
2020-10-26 14:58:45 +0000

    added check for script being run without arguments

--- mkclass.sh
+++ mkclass.sh
@@ -1,17 +1,20 @@
 #!/usr/bin/env bash
 
-if [[ $1 = "help" ]]; then
+if [[ $# == 0 ]]; then
+  echo "Type mkclass help for usage instructions"
+fi
+
+if [[ $1 == "help" ]]; then
   echo "Type mkclass [name_of_class] [language] (languages can currently be -cpp or -py) Please refer to README for more info"
 fi 
 
-if [[ $2 = "-cpp" ]]; then # Create a C++ class
+if [[ $2 == "-cpp" ]]; then # Create a C++ class
 	touch "$1.cpp"
 	touch "$1.hpp"
 	echo "#include \"$1.hpp\"" >> "$1.cpp"
-	printf "#ifndef %s_HPP\n#define %s_HPP\n\nclass %s {\n\n}\n\n#endif //%s_HPP" "${1^^}" "${1^^}" "$1" "${1^^}" >> "$1.hpp"
-fi 
+	printf "#ifndef %s_HPP\n#define %s_HPP\n\nclass %s {\n\n};\n\n#endif //%s_HPP" "${1^^}" "${1^^}" "$1" "${1^^}" >> "$1.hpp"
 
-if [[ $2 = "-py" ]]; then
+elif [[ $2 == "-py" ]]; then
   touch "$1.py" 
   echo "class $1:" >> "$1.py"
 fi
