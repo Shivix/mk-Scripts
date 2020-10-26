e43f64dca6e203cb960fcc555b5fab741558609b
Shivix <markoborne1@gmail.com>
2020-10-26 12:03:16 +0000

    insert contents of hpp files

--- mkclass.sh
+++ mkclass.sh
@@ -1,7 +1,13 @@
 #!/usr/bin/env bash
 
-if [[ $2 = '-cpp' ]]; then
+if [[ $2 = '-cpp' ]]; then # Create a C++ class
 	touch "$1.cpp"
 	touch "$1.hpp"
 	echo "#include \"$1.hpp\"" >> "$1.cpp"
-fi
+	printf "#ifndef %s_HPP\n
+	        #define %s_HPP\n\n
+	        class %s {\n
+	        \n
+	        }\n
+	        #endif //%s_HPP" "$1" "$1" "$1" "$1" >> "$1.hpp"
+fi 
