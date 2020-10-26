1b23ce6ef7e5bcfdb05c5071c108fde353eb1bbe
Shivix <markoborne1@gmail.com>
2020-10-26 18:46:49 +0000

    Added comment

--- mkclass.sh
+++ mkclass.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-if [[ $# == 0 ]]; then
+if [[ $# == 0 ]]; then # checks if the command was run without arguments
   echo "Type mkclass help for usage instructions"
 fi
 
@@ -16,7 +16,7 @@ if [[ $2 == "-cpp" ]]; then # Create a C++ class
 	echo "#include \"$1.hpp\"" >> "$1.cpp"
 	printf "#ifndef %s_HPP\n#define %s_HPP\n\nclass %s {\n\n};\n\n#endif //%s_HPP" "${1^^}" "${1^^}" "$1" "${1^^}" >> "$1.hpp"
 
-elif [[ $2 == "-py" ]]; then
+elif [[ $2 == "-py" ]]; then # Create a python class
   touch "$1.py" 
   echo "class $1:" >> "$1.py"
 fi
