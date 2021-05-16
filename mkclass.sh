813e19d76e6b1c555afbed431ff851d94c9b38bd
Mark Oborne <markoborne1@gmail.com>
2021-05-16 01:42:16 +0100

    now defaults to cpp if no language argument provided

--- mkclass.sh
+++ mkclass.sh
@@ -2,21 +2,44 @@
 
 if [[ $# == 0 ]]; then # checks if the command was run without arguments
   echo "Type mkclass help for usage instructions"
+  exit
 fi
 
+
 if [[ $1 == "help" ]]; then
-  echo "Type mkclass [name_of_class] [language] (languages can currently be -cpp or -py) Please refer to README for more info"
+  echo "Type mkclass [name_of_class] [language] (languages can currently be -cpp or -py)"
 elif [[ $# == 1 ]]; then
-  echo "Please add a language argument. Type mkclass help for more info."
+	file_type="-cpp"
+else
+	file_type=$2
 fi 
 
-if [[ $2 == "-cpp" ]]; then # Create a C++ class
-	touch "$1.cpp"
-	touch "$1.hpp"
-	echo "#include \"$1.hpp\"" >> "$1.cpp"
-	printf "#ifndef %s_HPP\n#define %s_HPP\n\nclass %s {\n\n};\n\n#endif //%s_HPP" "${1^^}" "${1^^}" "$1" "${1^^}" >> "$1.hpp"
+if [[ $file_type == "-cpp" ]]; then # Create a C++ class
+	while [ ! -f CMakeLists.txt ]
+	do
+		if [ -d bin ]; then
+			echo "Failed to find CMakeLists.txt"
+			exit -1;
+		fi
+		cd ..
+	done
 
-elif [[ $2 == "-py" ]]; then # Create a python class
+	touch src/$1.cpp
+	touch include/$1.hpp
+	echo "#include \"include/$1.hpp\"" >> "src/$1.cpp"
+
+	guard_name=${PWD##*/}_$1_hpp
+
+cat > include/$1.hpp << EOF
+#ifndef ${guard_name}
+#define ${guard_name}
+
+
+
+#endif //${guard_name}
+EOF
+
+elif [[ $file_type == "-py" ]]; then # Create a python class
   touch "$1.py" 
   echo "class $1:" >> "$1.py"
 fi
