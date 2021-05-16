b10faba9048ee0a7dcb8f0c8703b81cc31abdbc9
Mark Oborne <markoborne1@gmail.com>
2021-05-16 01:44:47 +0100

    added script to easily chmod all the scripts and add them to /usr/local/bin

---
+++ install.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env bash
+
+chmod +x mkclass.sh
+sudo cp mkclass.sh /usr/local/bin/mkclass.sh
+
+chmod +x mkheader.sh
+sudo cp mkheader.sh /usr/local/bin/mkheader.sh
+
+chmod +x mkproject.sh
+sudo cp mkproject.sh /usr/local/bin/mkproject.sh
+
+chmod +x mktest.sh
+sudo cp mktest.sh /usr/local/bin/mktest.sh
